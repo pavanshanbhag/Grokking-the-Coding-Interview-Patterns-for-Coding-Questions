7c1342df32e67b4c3b80572bd5ddaa35d1b94d3b
Pavan Shanbhag <pavan.shanbhag@intel.com>
2024-11-05 15:04:36 +0530

    refactor:: topological sort pattern

--- 1. Pattern Sliding Window/Problem Challenge 1 - Permutation in a String (hard) .py
+++ 1. Pattern Sliding Window/Problem Challenge 1 - Permutation in a String (hard) .py
@@ -42,7 +42,7 @@ Explanation: The string contains "acb" which is a permutation of the given patte
 
 
 # answer
-def find_permutation(str, pattern):
+def find_permutation(s, pattern):
     window_start, matched = 0, 0
     char_frequency = {}
 
@@ -53,8 +53,8 @@ def find_permutation(str, pattern):
 
     # our goal is to match all the characters from the 'char_frequency' with the current window
     # try to extend the range [window_start, window_end]
-    for window_end in range(len(str)):
-        right_char = str[window_end]
+    for window_end in range(len(s)):
+        right_char = s[window_end]
         if right_char in char_frequency:
             # decrement the frequency of matched character
             char_frequency[right_char] -= 1
@@ -66,7 +66,7 @@ def find_permutation(str, pattern):
 
         # shrink the window by one character
         if window_end >= len(pattern) - 1:
-            left_char = str[window_start]
+            left_char = s[window_start]
             window_start += 1
             if left_char in char_frequency:
                 if char_frequency[left_char] == 0:
